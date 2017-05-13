6b0b63919109fbbb370e3dfdb4c45628a04b7545
Anton Khorev <tony29@yandex.ru>
2017-05-13 13:58:51 +0300

    slider changes threshold

--- map.js
+++ map.js
@@ -1,23 +1,19 @@
-L.Control.ColorSlider=L.Control.extend({
-	options: {
-		position: 'bottomleft'
-	},
-	onAdd: function(){
-		var div=L.DomUtil.create('div','leaflet-control-layers')
-		var slider=L.DomUtil.create('input','',div)
-		slider.setAttribute('type','range')
-		slider.setAttribute('min',0)
-		slider.setAttribute('max',36)
-		slider.setAttribute('value',6)
-		L.DomEvent.on(slider,'change',function(ev){
-			console.log('changed!',slider.value)
-		})
-		L.DomEvent.disableClickPropagation(div)
-		return div
+var defaultColorThreshold=6, maxColorThreshold=36
+function computePolygonColor(age,threshold) {
+	var month=1000*60*60*24*30
+	var span1=threshold*month, span2=maxColorThreshold*month
+	var hotness,hotnessPercent
+	if (age<span1) {
+		hotness=(span1-age)/span1
+		hotnessPercent=Math.round(100*hotness)
+		return "rgb("+hotnessPercent+"%,0%,"+(100-hotnessPercent)+"%)"
+	} else if (age<span2) {
+		hotness=(span2-age)/(span2-span1)
+		hotnessPercent=Math.round(100*hotness)
+		return "rgb(0%,0%,"+hotnessPercent+"%)"
+	} else {
+		return "#000"
 	}
-})
-L.control.colorSlider=function(options){
-	return new L.Control.ColorSlider(options)
 }
 
 var div=document.getElementById('map')
@@ -26,6 +22,7 @@ var map=L.map(div).addLayer(L.tileLayer(
 	'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 	{attribution: "© <a href=https://www.openstreetmap.org/copyright>Участники OpenStreetMap</a>"}
 ))
+var now=Date.now()
 var segmentLayer=L.featureGroup(data.map(function(segment){
 	var popupHtml=
 		"<strong>"+segment.n+"</strong><br>"+segment.d+"<br><br>"+
@@ -33,23 +30,34 @@ var segmentLayer=L.featureGroup(data.map(function(segment){
 	if (segment.c.length>0) {
 		popupHtml+=", записано в пакете <a href=https://www.openstreetmap.org/changeset/"+segment.c+">"+segment.c+"</a>"
 	}
-	var date=Date.parse(segment.t)
-	var now=Date.now()
-	var month=1000*60*60*24*30
-	var span1=6*month, span2=36*month
-	var hotness,hotnessPercent,polygonColor
-	if (now-date<span1) {
-		hotness=(date-(now-span1))/span1
-		hotnessPercent=Math.round(100*hotness)
-		polygonColor="rgb("+hotnessPercent+"%,0%,"+(100-hotnessPercent)+"%)"
-	} else if (now-date<span2) {
-		hotness=(date-(now-span2))/(span2-span1)
-		hotnessPercent=Math.round(100*hotness)
-		polygonColor="rgb(0%,0%,"+hotnessPercent+"%)"
-	} else {
-		polygonColor="#000"
-	}
-	return L.polygon(segment.p,{color:polygonColor}).bindPopup(popupHtml)
+	var age=now-Date.parse(segment.t)
+	var segmentPolygon=L.polygon(segment.p,{color:computePolygonColor(age,defaultColorThreshold)}).bindPopup(popupHtml)
+	segmentPolygon.age=age
+	return segmentPolygon
 })).addTo(map)
 map.fitBounds(segmentLayer.getBounds())
+
+L.Control.ColorSlider=L.Control.extend({
+	options: {
+		position: 'bottomleft'
+	},
+	onAdd: function(){
+		var div=L.DomUtil.create('div','leaflet-control-layers')
+		var slider=L.DomUtil.create('input','',div)
+		slider.type='range'
+		slider.min=1
+		slider.max=maxColorThreshold-1
+		slider.value=defaultColorThreshold
+		L.DomEvent.on(slider,'change',function(ev){
+			segmentLayer.eachLayer(function(segmentPolygon){
+				segmentPolygon.setStyle({color:computePolygonColor(segmentPolygon.age,slider.value)})
+			})
+		})
+		L.DomEvent.disableClickPropagation(div)
+		return div
+	}
+})
+L.control.colorSlider=function(options){
+	return new L.Control.ColorSlider(options)
+}
 L.control.colorSlider().addTo(map)
